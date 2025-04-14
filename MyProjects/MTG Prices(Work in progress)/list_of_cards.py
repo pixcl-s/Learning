389e9a8efdddcaaa9983f6c6ece95b5d09da32b8
Martin Simeonov <the.hun73r@gmail.com>
2025-04-14 11:29:52 +0300

    Add files via upload

---
+++ MyProjects/MTG Prices(Work in progress)/list_of_cards.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+import interface
+import commands
+from cards_info import card_info
+
+card_info = card_info
+
+
+class ListOfCards(interface.Frames):
+    def __init__(self, main: "Main"):
+        super().__init__(main)
+
+        self.list = tk.Listbox(self.frame, bg="grey22", fg="snow", activestyle="none", selectforeground="black", font=('Tahoma', 15))
+        self.add_button = tk.Button(self.frame, text="Add", command=commands.add_card, bg="grey26", fg="snow", width=10)
+        self.remove_button = tk.Button(self.frame, text="Remove", command=commands.remove_cards, bg="grey26", fg="snow", width=10)
+
+        self.frame.columnconfigure(0, weight=1)
+        self.frame.rowconfigure(0, weight=1)
+
+        for index, card in enumerate(card_info):
+            self.list.insert(index, card)
+
+    def grid(self) -> None:
+        self.frame.grid(row=1, column=4, sticky="nsew")
+        self.list.grid(row=0, column=0, sticky="nsew")
+        self.add_button.grid(row=1, column=0, sticky="w")
+        self.remove_button.grid(row=1, column=0, sticky="e")
+
+
+list_of_cards_frame = ListOfCards(interface.app)
+list_of_cards_frame.grid()
---
+++ MyProjects/MTG Prices(Work in progress)/prices.py
@@ -0,0 +1,70 @@
+
+import requests
+
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import undetected_chromedriver as uc
+
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"}
+
+
+def cardkingdom_price(card_name_url):
+    url = "https://json.edhrec.com/cards/"
+
+    transformed_url = requests.get(card_name_url, headers=headers).url
+    card_name = transformed_url.split("/")[-1]
+    response = requests.get(url + card_name, headers=headers)
+    data = response.json()
+    sleep(0.5)
+    return data["prices"]["cardkingdom"]["price"]
+
+
+def tcgplayer_price(url):
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument(f"user-agent={headers['User-Agent']}")
+
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get(url)
+
+    price_element = WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.XPATH, '//span[contains(@class, "spotlight__price")]')))
+
+    sleep(10)
+    price = price_element.text
+    driver.quit()
+
+    return price
+
+
+def cardmarket_price(url):
+    options = uc.ChromeOptions()
+    # options.add_argument("--headless")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-gpu")
+
+    driver = uc.Chrome(options=options)
+    driver.get(url)
+
+    sleep(5)
+    driver.save_screenshot("screenshot.png")
+    cookie_button = driver.find_element(By.XPATH, "//button[contains(text(), 'Accept')]")
+    cookie_button.click()
+    sleep(2)
+
+    data = driver.find_elements(By.TAG_NAME, "dd")[4:7]
+
+    info = dict(zip(["available_items", "lowest_price", "price_trend"], [x.text for x in data]))
+
+    driver.quit()
+
+    return info
